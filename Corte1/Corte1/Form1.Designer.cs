472c010cd9ac8c288c52afcf71f15147fae4eb2c
priscila0621 <pjulieths@uamv.edu.ni>
2024-09-23 08:48:19 -0600

    Final Commit Corte1

--- Corte1/Corte1/Form1.Designer.cs
+++ Corte1/Corte1/Form1.Designer.cs
@@ -28,13 +28,151 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.tbNames = new System.Windows.Forms.TextBox();
+            this.tbLastNames = new System.Windows.Forms.TextBox();
+            this.dtpBirthdate = new System.Windows.Forms.DateTimePicker();
+            this.cbCity = new System.Windows.Forms.ComboBox();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnCalculate = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(38, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(229, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Regisitro de edades - PJSF";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 60);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(52, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Nombres:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 90);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(52, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Apellidos:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(17, 128);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(111, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Fecha de Nacimiento:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(15, 157);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(43, 13);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Ciudad:";
+            // 
+            // tbNames
+            // 
+            this.tbNames.Location = new System.Drawing.Point(72, 57);
+            this.tbNames.Name = "tbNames";
+            this.tbNames.Size = new System.Drawing.Size(90, 20);
+            this.tbNames.TabIndex = 5;
+            // 
+            // tbLastNames
+            // 
+            this.tbLastNames.Location = new System.Drawing.Point(75, 85);
+            this.tbLastNames.Name = "tbLastNames";
+            this.tbLastNames.Size = new System.Drawing.Size(101, 20);
+            this.tbLastNames.TabIndex = 6;
+            // 
+            // dtpBirthdate
+            // 
+            this.dtpBirthdate.Location = new System.Drawing.Point(146, 121);
+            this.dtpBirthdate.Name = "dtpBirthdate";
+            this.dtpBirthdate.Size = new System.Drawing.Size(201, 20);
+            this.dtpBirthdate.TabIndex = 7;
+            // 
+            // cbCity
+            // 
+            this.cbCity.FormattingEnabled = true;
+            this.cbCity.Location = new System.Drawing.Point(66, 161);
+            this.cbCity.Name = "cbCity";
+            this.cbCity.Size = new System.Drawing.Size(185, 21);
+            this.cbCity.TabIndex = 8;
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(45, 203);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(130, 44);
+            this.btnAdd.TabIndex = 9;
+            this.btnAdd.Text = "Agregar Datos";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // btnCalculate
+            // 
+            this.btnCalculate.Location = new System.Drawing.Point(200, 203);
+            this.btnCalculate.Name = "btnCalculate";
+            this.btnCalculate.Size = new System.Drawing.Size(127, 42);
+            this.btnCalculate.TabIndex = 10;
+            this.btnCalculate.Text = "Calcular Edad";
+            this.btnCalculate.UseVisualStyleBackColor = true;
+            this.btnCalculate.Click += new System.EventHandler(this.btnCalculate_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(394, 292);
+            this.Controls.Add(this.btnCalculate);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.cbCity);
+            this.Controls.Add(this.dtpBirthdate);
+            this.Controls.Add(this.tbLastNames);
+            this.Controls.Add(this.tbNames);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Registro de edades - PJSF";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox tbNames;
+        private System.Windows.Forms.TextBox tbLastNames;
+        private System.Windows.Forms.DateTimePicker dtpBirthdate;
+        private System.Windows.Forms.ComboBox cbCity;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnCalculate;
     }
 }
 
--- Corte1/Corte1/Form1.cs
+++ Corte1/Corte1/Form1.cs
@@ -7,14 +7,90 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Corte1.Models;
 
 namespace Corte1
 {
     public partial class Form1 : Form
     {
+        // Instancia de la clase Registro para almacenar las personas
+        private Registro registro = new Registro();
+
+        // Instancia de la clase Operacion para realizar cálculos sobre las personas
+        private Operacion operacion = new Operacion();
+
         public Form1()
         {
             InitializeComponent();
+            // Añade una lista de ciudades al ComboBox
+            cbCity.Items.AddRange(new string[] { "Managua", "Granada", "Masaya" });
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            // Validación: Verificar que los nombres no estén vacíos
+            if (string.IsNullOrWhiteSpace(tbNames.Text))
+            {
+                MessageBox.Show("Por favor, ingrese un nombre.");
+                return;
+            }
+
+            // Validación: Verificar que los apellidos no estén vacíos
+            if (string.IsNullOrWhiteSpace(tbLastNames.Text))
+            {
+                MessageBox.Show("Por favor, ingrese un apellido.");
+                return;
+            }
+
+            // Validación: Verificar que una ciudad haya sido seleccionada
+            if (cbCity.SelectedItem == null)
+            {
+                MessageBox.Show("Por favor, seleccione una ciudad.");
+                return;
+            }
+
+            // Crea una nueva instancia de Persona con los datos ingresados
+            Persona persona = new Persona
+            {
+                Nombres = tbNames.Text,
+                Apellidos = tbLastNames.Text,
+                FechaNacimiento = dtpBirthdate.Value,
+                Ciudad = cbCity.SelectedItem.ToString()
+            };
+
+            // Agrega la persona al registro
+            registro.AgregarPersona(persona);
+
+            // Muestra un mensaje de confirmación
+            MessageBox.Show("Persona agregada con éxito.");
+        }
+
+        private void btnCalculate_Click(object sender, EventArgs e)
+        {
+            // Obtiene la última persona añadida al registro
+            Persona persona = registro.MostrarPersonas().LastOrDefault();
+
+            // Validación: Verificar que haya al menos una persona registrada
+            if (persona == null)
+            {
+                MessageBox.Show("No hay personas registradas.");
+                return;
+            }
+
+            // Calcula la edad de la persona
+            int edad = operacion.CalcularEdad(persona.FechaNacimiento);
+
+            // Determina si es mayor o menor de edad
+            bool esMayor = operacion.EsMayorDeEdad(persona.FechaNacimiento);
+
+            // Muestra un mensaje con la edad y si es mayor o menor de edad
+            MessageBox.Show($"{persona.Nombres} tiene {edad} años y es {(esMayor ? "mayor" : "menor")} de edad.");
         }
     }
+    
 }
